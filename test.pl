e80a7739ca3d8e8239d8465b8bc74a09583445a0
sharef <gamerzi@gmail.com>
2012-12-10 13:21:18 -0500

    Testing for Class::db

--- test.pl
+++ test.pl
@@ -14,5 +14,18 @@ my $stuff = db->new;
 #my ($class, $arch) = @{$stats}{'base','name'};
 #print $class,$arch,Dumper($stats),Dumper($abilities[0]);
 
-my $info = $stuff->user('available tokens','NPC');
-print Dumper($info);
+#my $info = $stuff->user('available tokens','NPC');
+my $test = $stuff->register({
+register=>'true',
+username=>'sharef',
+password=>'7da63a1f4cb36fdb5d58bbfbdfbbfb5cc05661e5bd4f3a91eb4ba16146e71c8b',
+password2=>'7da63a1f4cb36fdb5d58bbfbdfbbfb5cc05661e5bd4f3a91eb4ba16146e71c8b',
+email=>'gamerzi%40gmail.com',
+gm=>'NPC',
+auth=>'1537',
+character=>'sharef',
+system=>'pf'
+});
+
+print Dumper($test);
+
