a089aeaae26bb6f02f1a27eb2a361ad7a9d3def3
sharef <gamerzi@gmail.com>
2012-12-10 13:17:54 -0500

    Registration almost working,  in-progress work on db->register

--- library/Class/db.pm
+++ library/Class/db.pm
@@ -1,12 +1,17 @@
 #!/usr/bin/perl
 package db;
-use Class;
-use base qw/Class/;
-use DBI;
+
 use warnings;
 use strict;
+
+use Class;
+use base qw/Class/;
+
+use DBI;
 use Data::Dumper;
 use Switch;
+use Digest::SHA 'sha256_hex';
+
 
 
 sub new {
@@ -91,63 +96,90 @@ sub user {
    } else { keys %$queries }
 }
 
-sub register {
+sub register (\%){
 
-   my ($self, $type, @_args) = @_;
+   my ($self, $_args) = @_;
 
    my $queries = {
       check => "
-         SELECT 
-            source, 
-            code, 
-            target, 
-            flag
-         FROM auth_codes
-         JOIN users 
-            ON users.id = auth_codes.source
-         WHERE users.name = (?)
-         AND code = (?)
-         AND flag = 'Register'
-         AND target IS NULL",
+         SELECT code.*
+         FROM auth_codes AS code
+         JOIN users AS s
+            ON s.id = code.source
+         WHERE s.name = (?)
+         AND code.code = (?)
+         AND code.flag = 'Register'
+         AND code.target IS NULL",
 
       create => "
          INSERT INTO `users`
          (`name`, `password`, `salt`, `token`, `token_issue`, `email`, `games`)
-         VALUES ((?), (?), (?), (?), (?), (?), (?))",
+         VALUES (?, ?, ?, ?, ?, ?, ?)",
 
       claim => "
          UPDATE auth_codes AS code
          JOIN users AS u
             ON u.id = code.source 
-         SET code.target = (?)
-         WHERE u.name =  (?) 
-         AND `code`.`code` = (?)
-         AND `code`.`target` IS NULL"
+         SET code.target = ?
+         WHERE u.name =  ? 
+         AND code.code = ?
+         AND code.target IS NULL",
+         
+      claimcheck => "
+         SELECT code.* 
+         FROM auth_codes AS code
+         JOIN users AS u
+            ON u.id = code.source
+         WHERE code.target = ?
+         AND u.name = ?
+         AND code.code = ?",
+   };
+   my $check = $self->cursor->prepare( $queries->{'check'} );
+  
+   #does the user not exist yet?
+   if ( $self->user('user',$_args->{'username'})->{'name'} ne $_args->{'username'} ) {
+   
+      #is the code valid?
+      my $check = $self->cursor->prepare( $queries->{'check'} );
+      if ( $check->execute(@{$_args}{'gm','auth'}) == 1 ) {
 
-      };
-   #if there is a type request, do things
-   if ( $type ) {
-      #if the type submitted is available, do things
-      if ( exists $queries->{$type} ) {
-         #update and create queries are restricted, and should be handled differently
-         my $restricted = {'create',1,'claim',1};
-         if ( !(exists $restricted->{$type}) ) {
-            #prepare the query
-            #TODO move all prepare statements to something.... else, such that they aren't GC'd when the query is done
-            my $query = $self->cursor->prepare( $queries->{$type} );
-            
-            #if the execute succedes, return the info, otherwise, 404 it
-            my $out = ($query->execute(@_args)>=1) ? $query->fetchrow_hashref : {name=>'404'};
+         #register that guy!
+         my $reg = $self->cursor->prepare( $queries->{'create'} );
+         
 
-            #send the caller the hashref
-            return $out;
-         } else {
-            #updating and editing things is a restricted art,  the method should return what it inserted, so this will be a double-query system
-            die('cannot create yet')
-         }
-      } else { die('The '.$type.' query hasn\'t been set up yet') }
-   } else { keys %$queries }
-                           
+         #make salt
+         $_args->{'salt'} = int(rand(2**32));
+        
+         if ($_args->{'password'} eq $_args->{'password2'}) {
+            $_args->{'password'} = sha256_hex($_args->{password}.$_args->{'salt'});
+         } else { die('Password mismatch') }
+         
+         $_args->{'token_issue'} = time;
+
+         #Behold, convoluted token generation.  Instead of generating a random number, this effectivly guarntees unique tokens
+         $_args->{'token'} =  sha256_hex($_args->{'username'}.$_args->{'salt'}.$_args->{'token_issue'});
+         
+         print Dumper($_args);
+#         $reg->execute(@{$_args}{'username', 'password', 'salt', 'token', 'token_issue', 'email', 'system'})
+      
+         #prep the claim
+         my $claim = $self->cursor->prepare( $queries->{'claim'} );
+
+         #create the claim array
+         my @array = (2, @{$_args}{'gm','auth'});
+
+         #was the claim successful? should output a 1, to indicate 1 row changed
+         if ( $claim->execute(@array) == 1 ) {
+
+            #prep the claim-check, check allll the things!
+            my $claimcheck = $self->cursor->prepare( $queries->{'claimcheck'} );
+                     
+            $claimcheck->execute(@array);
+         #   return $claimcheck->fetchrow_hashref,$_args;
+
+         } else { return } #could not claim
+      } else { return } #no code
+   } else { return } #user already exists
 }
 
 
