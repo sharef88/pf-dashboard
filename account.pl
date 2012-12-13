fe52d009fab43721e041983b3abc5ac9b0341058
sharef <gamerzi@gmail.com>
2012-12-13 10:50:28 -0500

    Some formatting and additional checks for valid session

--- account.pl
+++ account.pl
@@ -23,9 +23,16 @@ my @input;
 
 #eat the error code from bad json codes
 eval { 
-   @input = $q->param('session') ? @{decode_json($q->param('session'))} : @{[404,0,0]}; 
+   if ( exists $q->Vars->{'session'} ) {
+      @input =  @{decode_json($q->param('session'))} 
+   } else {
+      print "<script type=text/javascript>
+         \$(this).logout();
+         </script>";
+      exit;
+   }
 };
-#regurgitate the error as a 500, then cause a logout, which will clear the session value;
+#regurgitate the error as a 500, then cause a logout, which will clear the session value, thus clearing the bad json;
 if ( $@ ) {
    @input = @{[500]}; #pointless, but may be useful later
    print "<script type=text/javascript>
@@ -34,39 +41,61 @@ if ( $@ ) {
    exit;
 }
 
-#session checking
+#Get the corresponding user to the session id
 my $user = $db->user('sessioncheck',$input[1],time-$Default_Timeout);
 
-
-unless ( $q->param('login') ) {
-   print $q->Link({ href=> 'css/account.css', rel=>'stylesheet',type=>'text/css' });
-    
-   print $q->start_div( { id => 'account_tabs' } ),
-      $q->ul(
-        { id => 'account_menu' },
-        $q->li(
-            { class => '.account_menu' },
-            $q->a( { href => '#account_personal'}, 'Personal Details' ) 
-        ),
-        $q->li(
-            { class => '.account_menu' },
-            $q->a( { href => '#account_game'}, 'Game Preferences' )
-        ),
-      ),
-      $q->div({id=>'account_personal'},
-         "You are $user->{name}<p>
-         Dis is all unformatted, so shaddup"
-      ),
-      $q->div({id=>'account_game'},
-         "This is the 'Game Preferences tab'<p>You prefer $user->{games}"
-      ),
-      $q->end_div();
-
-
-
-    
-
-   print $q->script({type=>'text/javascript', src=>'js/account.js'});
-
+#if the user comes back as a 404, the session was invalid, force-logout to clear session
+if ( $user->{name} eq '404' ) {
+   print "<script type=text/javascript>
+      \$(this).logout();
+      </script>";
+   exit;
 }
+
+
+
+#--------------------------------------------#
+#+++++++++++ Print the actual page ++++++++++#
+#--------------------------------------------#
+
+
+#first the stylesheet
+print $q->Link({ href=> 'css/account.css', rel=>'stylesheet',type=>'text/css' });
+ 
+
+#account page
+print $q->start_div( { id => 'account_tabs' } ),
+   #Print the options
+   $q->ul(
+     { id => 'account_menu' },
+     #option 1: personal stuff
+     $q->li(
+         { class => '.account_menu' },
+         $q->a( { href => '#account_personal'}, 'Personal Details' ) 
+     ),
+     #option 2: game stuff
+     $q->li(
+         { class => '.account_menu' },
+         $q->a( { href => '#account_game'}, 'Game Preferences' )
+     ),
+   ), #end ul#account_menu
+   
+   #Print the actual pages
+   $q->div({id=>'account_personal'},
+      "You are $user->{name}<p>
+      Dis is all unformatted, so shaddup"
+   ), #end div#account_personal
+   
+   $q->div({id=>'account_game'},
+      "This is the 'Game Preferences tab'<p>You prefer $user->{games}"
+   ),#end div#account_game
+   
+   $q->end_div(); #end div#account_tabs
+
+
+
+ 
+
+print $q->script({type=>'text/javascript', src=>'js/account.js'});
+
 
