0b450ee84b8bc20293fd843db4fb0f97afffb47f
sharef <gamerzi@gmail.com>
2012-12-26 17:04:11 -0500

    dbix testing, iz gud

--- test.pl
+++ test.pl
@@ -6,6 +6,8 @@ use lib qw/library/;
 use Class::db::rules;
 use Class::db::user;
 use util;
+use pfdash::Schema;
+use config;
 
 use Data::Dumper;
 use CGI;
@@ -13,8 +15,10 @@ use MIME::Lite;
 
 my $q = CGI->new;
 
-my $rules = rules->new;
-my $user = user->new({name=>'sharef'});
+#my $rules = rules->new;
+#my $user = user->new({name=>'sharef'});
+my $schema = pfdash::Schema->connect("dbi:mysql:dbname=$config::db", $config::user, $config::pw);
+$schema->storage->debug(1);
 
 my $msg = MIME::Lite->new( 
    From => 'admin@orbanos.org', 
@@ -26,7 +30,14 @@ my $msg = MIME::Lite->new(
 
 #$msg->send;
 
-print sha256_hex($q->param('password').$user->{'salt'});
-print Dumper($user->user('id',2));
 
-#print Dumper(@stuff);
+#my $arch1 = $schema->resultset('ArchList')->find({name=>'Monk'})->abilities;
+#print join ", ", $arch1->get_column('name')->func('distinct');
+my $user_options = $schema->resultset('User')->find({name=>'sharef'})->options;
+print join ", ", $user_options->first->option_value;
+
+
+
+
+
+
